a13afc0736cc637bb15bc4103d28d9779ccb66ef
dedrowe <barushnikovvs@gmail.com>
2025-03-13 14:14:20 +0400

    feature: присоединил докер
    добавил docker-compose.yml с базой данных и миграциями, соответствующий файл с конфигурацией, а так же обновил конфигурацию скраппера для работы с postgresql

---
+++ migrations/00-initial-schema.sql
@@ -0,0 +1,21 @@
+create table tg_chats (
+    id bigserial primary key,
+    chat_id bigint unique not null,
+    deleted boolean default false
+);
+
+create table links (
+    id bigserial primary key,
+    link varchar(512) unique not null,
+    last_update timestamp,
+    deleted boolean default false
+);
+
+create table links_data (
+    id bigserial primary key,
+    link_id bigserial references links (id) not null,
+    chat_id bigserial references tg_chats (id) not null,
+    tags varchar(16)[] not null default array[]::varchar[],
+    filters varchar(32)[] not null default array[]::varchar[],
+    deleted boolean default false
+);
--- scrapper/src/main/java/backend/academy/scrapper/ScrapperConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/ScrapperConfig.java
@@ -3,13 +3,15 @@ package backend.academy.scrapper;
 import backend.academy.shared.validation.url.Url;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ScrapperConfig(
-        @Valid GithubCredentials github, @Valid StackOverflowCredentials stackOverflow, @Valid Bot bot) {
+        @Valid GithubCredentials github, @Valid StackOverflowCredentials stackOverflow, @Valid Bot bot,
+        @NotNull DbAccessType accessType) {
 
     public record Bot(@NotEmpty String url) {}
 
@@ -17,4 +19,10 @@ public record ScrapperConfig(
 
     public record StackOverflowCredentials(
             @NotEmpty String key, @NotEmpty String accessToken, @NotEmpty @Url String SOBaseUrl) {}
+
+    public enum DbAccessType {
+        RAM,
+        SQL,
+        ORM
+    }
 }
