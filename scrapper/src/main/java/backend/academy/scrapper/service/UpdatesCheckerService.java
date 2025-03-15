fab7efdee6edc654b976b0ec4b593608a1e229d6
dedrowe <barushnikovvs@gmail.com>
2025-03-15 14:19:42 +0400

    refactor: переделал UpdatesCheckerService
    сделал получение ссылок по батчам, многопоточный поиск обновлений по ссылкам и добавил фильтрацию на ссылки, которые не проверялись определенный промежуток времени

--- scrapper/src/main/java/backend/academy/scrapper/service/UpdatesCheckerService.java
+++ scrapper/src/main/java/backend/academy/scrapper/service/UpdatesCheckerService.java
@@ -5,7 +5,6 @@ import static backend.academy.scrapper.utils.FutureUnwrapper.unwrap;
 import backend.academy.scrapper.entity.Link;
 import backend.academy.scrapper.entity.LinkData;
 import backend.academy.scrapper.entity.TgChat;
-import backend.academy.scrapper.exceptionHandling.exceptions.TgChatException;
 import backend.academy.scrapper.exceptionHandling.exceptions.WrongServiceException;
 import backend.academy.scrapper.mapper.LinkMapper;
 import backend.academy.scrapper.repository.link.LinkRepository;
@@ -17,13 +16,19 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +36,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @AllArgsConstructor
 public class UpdatesCheckerService {
+
+    private static final int DEFAULT_BATCH_SIZE = 200;
+
+    private static final int DEFAULT_THREAD_POOL_SIZE = 4;
+
+    private final int batchSize;
+
+    private final int threadPoolSize;
 
     private final LinkDispatcher linkDispatcher;
 
@@ -44,33 +57,83 @@ public class UpdatesCheckerService {
 
     private final TgChatRepository tgChatRepository;
 
+    @Autowired
+    public UpdatesCheckerService(
+            LinkDispatcher linkDispatcher,
+            LinkDataRepository linkDataRepository,
+            LinkRepository linkRepository,
+            LinkMapper linkMapper,
+            TgBotClient tgBotClient,
+            TgChatRepository tgChatRepository) {
+        this.linkDispatcher = linkDispatcher;
+        this.linkDataRepository = linkDataRepository;
+        this.linkRepository = linkRepository;
+        this.linkMapper = linkMapper;
+        this.tgBotClient = tgBotClient;
+        this.tgChatRepository = tgChatRepository;
+        this.batchSize = DEFAULT_BATCH_SIZE;
+        this.threadPoolSize = DEFAULT_THREAD_POOL_SIZE;
+    }
+
     @Scheduled(fixedRate = 1, timeUnit = TimeUnit.MINUTES)
     public void checkUpdates() {
-        List<Link> links = unwrap(linkRepository.getAll());
-
-        for (Link link : links) {
-            URI uri = URI.create(link.link());
-            ApiClientWrapper client = linkDispatcher.dispatchLink(uri);
-            LocalDateTime lastUpdate = client.getLastUpdate(uri);
-            if (lastUpdate.isAfter(link.lastUpdate())) {
-                link.lastUpdate(lastUpdate);
-                unwrap(linkRepository.update(link));
-
-                List<LinkData> linkDataList = unwrap(linkDataRepository.getByLinkId(link.id()));
-                List<Long> chatIds = new ArrayList<>();
-                CompletableFuture<Optional<TgChat>>[] futures = linkDataList.stream()
-                        .map(linkData -> tgChatRepository.getById(linkData.chatId()))
-                        .toArray(CompletableFuture[]::new);
-                for (int i = 0; i < futures.length; i++) {
-                    int finalI = i;
-                    chatIds.add(unwrap(futures[i])
-                            .orElseThrow(() -> new TgChatException(
-                                    "Чат с таким id не зарегистрирован", String.valueOf(linkDataList.get(finalI))))
-                            .chatId());
+        try (ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize)) {
+            long linksBatchesCount = 0;
+            while (true) {
+                List<Link> links = unwrap(linkRepository.getAllNotChecked(
+                        batchSize * linksBatchesCount, batchSize, LocalDateTime.now(ZoneOffset.UTC)));
+                if (links.isEmpty()) {
+                    break;
                 }
-                tgBotClient.sendUpdates(linkMapper.createLinkUpdate(
-                        link.id(), link.link(), "Получено обновление по ссылке " + link.link(), chatIds));
+
+                executorService.invokeAll(links.stream()
+                        .map(link -> (Callable<Void>) () -> checkUpdatesForLink(link))
+                        .toList());
+
+                ++linksBatchesCount;
             }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Void checkUpdatesForLink(Link link) {
+        URI uri = URI.create(link.link());
+        ApiClientWrapper client = linkDispatcher.dispatchLink(uri);
+        LocalDateTime lastUpdate = client.getLastUpdate(uri);
+        if (lastUpdate.isAfter(link.lastUpdate())) {
+            link.lastUpdate(lastUpdate);
+            unwrap(linkRepository.update(link));
+
+            checkUpdatesForLink(link.id(), link.link());
+        }
+        return null;
+    }
+
+    private void checkUpdatesForLink(long linkId, String link) {
+        long linksDataBatchesCount = 0;
+        while (true) {
+            List<LinkData> linkDataList =
+                    unwrap(linkDataRepository.getByLinkId(linkId, linksDataBatchesCount * batchSize, batchSize));
+            if (linkDataList.isEmpty()) {
+                break;
+            }
+            List<Long> chatIds = new ArrayList<>();
+            CompletableFuture<Optional<TgChat>>[] futures = linkDataList.stream()
+                    .map(linkData -> tgChatRepository.getById(linkData.chatId()))
+                    .toArray(CompletableFuture[]::new);
+            for (int i = 0; i < futures.length; i++) {
+                int finalI = i;
+                unwrap(futures[i]).ifPresentOrElse(tgChat -> chatIds.add(tgChat.chatId()), () -> {
+                    try (var ignored = MDC.putCloseable(
+                            "id", String.valueOf(linkDataList.get(finalI).chatId()))) {
+                        log.warn("Чат не найден");
+                    }
+                });
+            }
+            tgBotClient.sendUpdates(
+                    linkMapper.createLinkUpdate(linkId, link, "Получено обновление по ссылке " + link, chatIds));
+            ++linksDataBatchesCount;
         }
     }
 
