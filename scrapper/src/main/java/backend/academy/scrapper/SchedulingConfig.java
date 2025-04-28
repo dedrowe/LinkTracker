ed0a2fc3f7a731c9605892407b5b319cc97e6b8a
dedrowe <barushnikovvs@gmail.com>
2025-04-28 11:11:46 +0400

    fix: починил шедулеры

--- scrapper/src/main/java/backend/academy/scrapper/SchedulingConfig.java
+++ scrapper/src/main/java/backend/academy/scrapper/SchedulingConfig.java
@@ -2,8 +2,10 @@ package backend.academy.scrapper;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @Profile("!test")
+@EnableScheduling
 public class SchedulingConfig {
 }
