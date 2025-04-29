3a498b0a45f987a517ea593d482075c8d12d92b0
dedrowe <barushnikovvs@gmail.com>
2025-04-29 18:25:27 +0400

    refactor: переработал шедулеры
    вынес в отдельный пакет, логику UpdatesSendScheduler вынес в отдельный сервис, чтобы уменьшить транзакцию

---
+++ scrapper/src/main/java/backend/academy/scrapper/scheduling/UpdatesSendScheduler.java
@@ -0,0 +1,24 @@
+package backend.academy.scrapper.scheduling;
+
+import backend.academy.scrapper.service.UpdatesSendService;
+import java.util.concurrent.TimeUnit;
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UpdatesSendScheduler {
+
+    private final UpdatesSendService service;
+
+    @Transactional
+    @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
+    public void sendUpdates() {
+        boolean sent = true;
+        while (sent) {
+            sent = service.sendUpdates();
+        }
+    }
+}
