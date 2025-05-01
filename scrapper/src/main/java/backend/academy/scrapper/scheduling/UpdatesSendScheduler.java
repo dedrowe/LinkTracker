b061e163501c327fe6736a84f35b49f4ed33ffa1
dedrowe <barushnikovvs@gmail.com>
2025-05-02 03:14:02 +0400

    fix: убрал транзакцию с шедулера

--- scrapper/src/main/java/backend/academy/scrapper/scheduling/UpdatesSendScheduler.java
+++ scrapper/src/main/java/backend/academy/scrapper/scheduling/UpdatesSendScheduler.java
@@ -5,7 +5,6 @@ import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -13,7 +12,6 @@ public class UpdatesSendScheduler {
 
     private final UpdatesSendService service;
 
-    @Transactional
     @Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
     public void sendUpdates() {
         boolean sent = true;
